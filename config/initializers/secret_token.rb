71c378c1df034d9a4e05d2b9601e1e505b902396
Eddie <edward.washington10@gmail.com>
2013-04-06 12:21:56 -0400

    Social Equity

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialEquity::Application.config.secret_token = '80842a75c60e4ebe5b2102ef942a6397e570d7ced72891d20f33558c072dde3266c5e17c0682f6afb7144a2e79bf073aaae8554e9003aabdf72f90857b0dc48d'
